0520dab800fa5c2e99fa7a08da5325511ca30897
Johnahan-web <j.tuckerjrmt@gmail.com>
2024-10-17 11:09:52 -0400

    commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const head = document.head
+head.append("Freelancer Forum")
+
+
+const freelancers = (
+    { name: "Dr. Slice", price: 25, occupation: "gardener" },
+    { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+    { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+    { name: "Prof. Prism", price: 81, occupation: "teacher" },
+    { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+    { name: "Prof. Spark", price: 76, occupation: "programmer" },
+    { name: "Dr. Wire", price: 47, occupation: "teacher" },
+    { name: "Prof. Goose", price: 72, occupation: "driver" },
+);
+  const init = () => {}
+    const divz_3 = document.createElement ("div");
+
+    const h1_1 = document. createElement ("h1"); 
+    const h1_2 = document.createElement ("h1");  
+
+    const h3_1 = document.createElement("h3");
+    const h3_2= document.createElement("h3"); 
+     const h3_3 = document.createElement ("h3");
+
+     h1_1.innerText = `Freelancer Forum`;
+     h1_2. innerText = `Available Freelancers`;
+    div2_1.innerText = `Name`;
+     div2_2.innerText = `Occupation`;
+     div_3.innerText = `Starting Price`;
+
+
+     divi. append (h1_1); 
+    section.append (div1); 
+    body-append (section1);
+
+    div1. append (h1_2) ;
+    section?.style.display - "flex";
+    section2.append(div2);
+    section2, append (div2_1);
+    section2. append (div2_2);
+    section2-append (div2_3);
+    body ,append (section2) ; 
+     
+    for (var i = 0; i < 5; i++) {
+      setTimeout(() => {
+        console, log(` Value of i: $(i)`);
+        }, i + 1000); 
+     }
+  
+     init();
